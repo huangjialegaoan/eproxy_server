231fb20574ee804c802e8b13e659c101ebfc4297
huangjiale <huangjl@blizzmi.com>
2017-10-10 13:12:15 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 echo heroku name:
 read name
-heroku create $name -s cedar
+heroku create $name -s cedar-14
 heroku config:add BUILDPACK_URL="https://github.com/heroku/heroku-buildpack-erlang.git"
 git push heroku master
